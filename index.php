d1eda0d12ecae1ee9b31d03cfaacecfc996bc4d3
mashsun <37840142+mashsun@users.noreply.github.com>
2019-02-07 14:56:50 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 						<div class="banner-content col-lg-10 col-md-12 justify-content-center">
 							<h6 class="text-uppercase">Whenever we bake, bake with our heart</h6>
 							<h1>
-								Love with Rapid Grocery
+							Love with Rapid Grocery
 							</h1>
 							<p class="text-white mx-auto">
 								Since Americans in the South began roasting pigs publicly, Barbecues have been a staple of North American living. For many, grilling becomes a routine mealtime activity.
 							</p>
-							<a href="#" class="primary-btn squire text-uppercase mt-10">Check Our Menu</a>
+							<a href="shop.php" class="primary-btn squire text-uppercase mt-10">Check Our Menu</a>
 						</div>
 					</div>
 				</div>
@@ -36,7 +36,7 @@
 								<div class="thumb">
 									<img class="img-fluid" src="img/c1.jpg" alt="">
 								</div>
-								<a href="#"><h4>Fresh</h4></a>
+								<a href="shop.php#Fresh"><h4>Fresh</h4></a>
 								<p>
 									inappropriate behavior is often laughed off as “boys will be.
 								</p>
@@ -47,7 +47,7 @@
 								<div class="thumb">
 									<img class="img-fluid" src="img/c2.jpg" alt="">
 								</div>
-								<a href="#"><h4>Bread</h4></a>
+								<a href="shop.php#Bakery"><h4>Bakery</h4></a>
 								<p>
 									inappropriate behavior is often laughed off as “boys will be.
 								</p>
@@ -58,7 +58,7 @@
 								<div class="thumb">
 									<img class="img-fluid" src="img/c3.jpg" alt="">
 								</div>
-								<a href="#"><h4>Meat</h4></a>
+								<a href="shop.php#Meat"><h4>Meat</h4></a>
 								<p>
 									inappropriate behavior is often laughed off as “boys will be.
 								</p>
@@ -69,164 +69,20 @@
 								<div class="thumb">
 									<img class="img-fluid" src="img/c4.jpg" alt="">
 								</div>
-								<a href="#"><h4>Dairy</h4></a>
+								<a href="shop.php#Dairy"><h4>Dairy</h4></a>
 								<p>
 									inappropriate behavior is often laughed off as “boys will be.
 								</p>
 							</div>
 						</div>
-						<a class="primary-btn mx-auto mt-80" href="#">View Full Menu</a>
+						<a class="primary-btn mx-auto mt-80" href="shop.php">View Full Menu</a>
 					</div>
 				</div>
 			</section>
 			<!-- End item-category Area -->
 
 			<!-- Start review Area -->
-			<section class="review-area section-gap relative">
-				<div class="overlay overlay-bg"></div>
-				<div class="container">
-					<div class="row d-flex justify-content-center">
-						<div class="col-md-9 pb-40 header-text text-center">
-							<h1 class="pb-10 text-white">Enjoy our Client’s Review</h1>
-							<p>
-								Who are in extremely love with eco friendly system.
-							</p>
-						</div>
-					</div>
-					<div class="row">
-						<div class="active-review-carusel">
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Fannie Rowe</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Hulda Sutton</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Fannie Rowe</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Hulda Sutton</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Fannie Rowe</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Hulda Sutton</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Fannie Rowe</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-							<div class="single-review item">
-								<img src="img/r1.png" alt="">
-								<div class="title justify-content-start d-flex">
-									<a href="#"><h4>Hulda Sutton</h4></a>
-									<div class="star">
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star checked"></span>
-										<span class="fa fa-star"></span>
-										<span class="fa fa-star"></span>
-									</div>
-								</div>
-								<p>
-									Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+			<?php include 'review.html';?>
 			<!-- End review Area -->
 
 <?php include 'footer.html';?>
