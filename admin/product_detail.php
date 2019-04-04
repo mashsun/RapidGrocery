5e517b30a597fb281edbb5214f6508a2351d4287
komallkaur <37879641+komallkaur@users.noreply.github.com>
2019-04-04 18:00:03 -0400

    Update product_detail.php

--- admin/product_detail.php
+++ admin/product_detail.php
@@ -1,3 +1,24 @@
+<?php
+include "config.php";
+
+// Check user login or not
+if(!isset($_SESSION['email'])){
+ }
+
+// logout
+if(isset($_POST['but_logout'])){
+    session_destroy();
+    header('Location: ../admin_login.php');
+}
+    $product_id = $_GET["product_id"];
+    $sql_query = "select p.product_id, p.product_name, ca.category_name, p.list_price, p.description, p.discount_percent, p.product_img
+                       from products p JOIN categories ca ON p.category_id = ca.category_id
+                       where p.product_id=$product_id
+                       group by p.product_id;";
+
+   $result = $con->query($sql_query);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,50 +41,77 @@
 
     <article>
         <h2>Product Details</h2>
+        <?php
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+                 while($rows = $result->fetch_assoc()) {
+                 $product_id = $rows["product_id"];
+                 $product_name = $rows["product_name"];
+                 $category = $rows["category_name"];
+                 $price = $rows["list_price"];
+                 $description = $rows["description"];
+                 $discount_percent = $rows["discount_percent"];
+                 $product_img = $rows["product_img"];
+                 }
+            }
+            else {
+                echo "0 results";
+            }
+        ?>
         <table id="customers">
             <tr>
                 <th width="250">Product ID</th>
-                <td>1</td>
+                <td><?php echo $product_id ?></td>
             </tr>
             <tr>
                 <th>Product Name</th>
-                <td>Donut</td>
+                <td><?php echo $product_name ?></td>
             </tr>
             <tr>
                 <th>Category</th>
-                <td>Bakery</td>
+                <td><?php echo $category ?></td>
             </tr>
             <tr>
                 <th>Price</th>
-                <td>$5</td>
+                <td><?php echo $price ?></td>
             </tr>
             <tr>
                 <th>Discription</th>
-                <td>
-                    The donut is popular in many countries and prepared in various forms as a sweet snack that can be homemade or purchased in bakeries.<br/>
-                    Total Fat 15g<br/>
-                    Saturated Fat 7g<br/>
-                    Trans Fat<br/>
-                    Cholesterol 0mg<br/>
-                    Sodium 350mg<br/>
-                    Total Carb 38g
-                </td>
+                <td><?php echo $description ?></td>
             </tr>
             <tr>
                 <th>Discount percent</th>
-                <td>5%</td>
+                <td><?php echo $discount_percent ?></td>
             </tr>
             <tr>
                 <th>Image Name</th>
-                <td>Bakery_002</td>
+                <td><?php echo $product_img ?></td>
             </tr>
         </table>
 
-        <p align="center"><input type="submit" value="Edit Product"> <input type="submit" value="Delete Product"></p>
+        <p align="center"><input type="submit" value="Edit Product" onclick="
+            <?php
+                echo "window.location='product_edit.php?product_id=" .$product_id."'";
+            ?> ;">
+
+        <input type="submit" value="Delete Product" onclick="
+            <?php
+                echo "window.location='javascript:del_Products(" .$product_id.")'";
+            ?> ;">
+        </p>
 
     </article>
 </section>
-
+<script>
+function del_Products(id)
+{
+    if(confirm("Are you sure want to delete this ?"))
+    {
+        document.location.href = "product_delete.php?product_id=" + id;
+     }
+}
+</script>
 <?php
 include "footer.php";
 ?>
